3df301acaf16e586a90f670b60d0a6af93e0c484
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2023-01-17 17:31:36 +0100

    fix: ci/cd and replace terser package

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import typescript from 'rollup-plugin-ts';
 import ttypescript from 'ttypescript';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/index.ts',

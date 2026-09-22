542b0949ab0bef6bd321b5ac6aeb05fd5253c42f
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2026-09-21 20:43:53 -0700

    chore(tooling): move to eslint 10, lomray configs 7 and lint-staged 17

--- release.config.js
+++ release.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-  branches: [
-    'prod',
-  ],
+  branches: ['prod'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
     '@semantic-release/github',
-  ]
-}
+  ],
+};
